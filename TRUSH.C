3abd0ae4aee31bdbe08e54d1156e2d998c53553b
tspatel <132879158+tspatel@users.noreply.github.com>
2023-05-11 13:46:11 +0530

    Add files via upload

---
+++ TRUSH.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+	{    int a=10,b=51,c;
+	     clrscr();
+	     c=a+b;
+	     printf("addition of a+b=%d",c);
+	     c=a-b;
+	     printf("\n substration of a-b=%d",c);
+	     c=a*b;
+	     printf("\nmultipliction of a*b=%d",c);
+	     c=a/b;
+	     printf("\ndivison of a/b=%d",c);
+	     c=a%b;
+	     printf("\nmodulas of a%b=%d",c);
+	     getch();
+	     }
